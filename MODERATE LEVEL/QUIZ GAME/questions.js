58fdab720dde8d3f855ec2390f7ca0cc79c726fa
Vamshikrishnareddy <67915861+vamshikrishna55@users.noreply.github.com>
2022-07-18 09:28:35 +0530

    Add files via upload

---
+++ MODERATE LEVEL/QUIZ GAME/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the average of first five multiples of 12?",
+    answer: "36",
+    options: [
+        "36",
+        "38",
+        "40",
+        "42",
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the compound interest on Rs. 2500 for 2 years at rate of interest 4% per annum?",
+    answer: "Rs. 204",
+    options: [
+        "Rs. 180",
+        "Rs. 204",
+        "Rs. 210",
+        "Rs. 220",
+    ]
+  },
+    {
+    numb: 3,
+    question: "If January 1, 1996, was Monday, what day of the week was January 1, 1997?",
+    answer: "Wednesday",
+    options: [
+      "Tuesday",
+      "Friday",
+      "Wednesday",
+      "Sunday"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the probability of getting an even number when a dice is rolled?",
+    answer: "1/2",
+    options: [
+      "1/5",
+      "1/2",
+      "1/4",
+      "1/6"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many times the hands of a clock coincide in a day?",
+    answer: "22",
+    options: [
+      "23",
+      "26",
+      "24",
+      "22"
+    ]
+  },
+];
